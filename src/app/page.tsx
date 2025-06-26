e2a8f384e4903ff6128b1786916fd70f8c7fe372
Eduardo Silva <eduardovinicios7856@gmail.com>
2025-06-26 14:46:27 +0000

    tire a chave api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,11 @@
 'use client';
 
-import { useAuth } from '@/context/auth-context';
-import { LoginClient } from '@/components/login-client';
 import { QuizClient } from '@/components/quiz-client';
-import { UserMenu } from '@/components/user-menu';
 
 export default function Home() {
-  const { user } = useAuth();
-
   return (
     <main className="flex min-h-screen w-full flex-col items-center justify-center p-4">
-      {user ? (
-        <>
-          <div className="absolute top-4 right-4">
-            <UserMenu />
-          </div>
-          <QuizClient />
-        </>
-      ) : (
-        <LoginClient />
-      )}
+      <QuizClient />
     </main>
   );
 }
